efa96ce8a0e3416dd4efa1589b3fab5052ad659c
Adrian <klimasa98@gmail.com>
2023-09-06 18:24:47 -0400

    download every var to js from html

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const addNewTaskBtn = document.querySelector('.add-new-task');
+const deleteAllTasksBtn = document.querySelector('.delete-all-tasks');
+
+const taskList = document.querySelector('.task-list');
+
+const newTaskTitle = document.querySelector('#task-title');
+const newTaskTime = document.querySelector('#task-time');
+
+const saveBtn = document.querySelector('.save-btn');
+const discardBtn = document.querySelector('.discard-btn');
+
+let ID = 0;
+
