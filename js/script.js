6f86446760b3703bf1f2a7a329fd1dc805e6e3a8
Adrian <klimasa98@gmail.com>
2023-09-07 19:17:45 -0400

    created function to delete single li from task list

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const addNewTaskBtn = document.querySelector('.add-new-task');
 const deleteAllTasksBtn = document.querySelector('.delete-all-tasks');
 
+const taskLeftTd = document.querySelector('.task-left-td');
+
+const error = document.querySelector('.error');
+
 const appBody = document.querySelector('.app-body');
 const taskList = document.querySelector('.task-list');
 
@@ -10,6 +14,10 @@ const newTaskTime = document.querySelector('#task-time');
 
 const saveBtn = document.querySelector('.save-btn');
 const discardBtn = document.querySelector('.discard-btn');
+
+const checkBtn = document.querySelector('.check-btn');
+const trashBtn = document.querySelectorAll('.trash-btn');
+const clockBtn = document.querySelector('.clock-btn');
 
 let ID = 0;
 
@@ -23,24 +31,52 @@ const addNewTask = () => {
 const clearInputs = () => {
 	newTaskTitle.value = '';
 	newTaskTime.value = '0';
+	error.style.display = 'none';
 };
 
 const createNewTask = () => {
-	const newTask = document.createElement('div');
-    ID++
+	if (newTaskTitle.value != '') {
+		const newTask = document.createElement('li');
 
-	newTask.innerHTML = `<li class="task" id="${ID}">
+		ID++;
+		newTask.setAttribute('ID', ID);
+		newTask.setAttribute('class', 'task');
+		newTask.innerHTML = `
     <p class="">${newTaskTitle.value} </p>
     <div class="btns">
         <button class="btn check-btn"> <i class="fa-solid fa-check"></i></button>
         <button class="btn trash-btn"><i class="fa-solid fa-trash"></i></button>
         <button class="btn clock-btn"><i class="fa-solid fa-clock"></i></button>
     </div>
-</li>`;
-	taskList.appendChild(newTask);
-    addNewTask()
+`;
+		taskList.appendChild(newTask);
+		addNewTask();
+		taskLeftTd.textContent = ID;
+	} else {
+		error.style.display = 'inline';
+	}
+};
+
+const deleteAllTasks = () => {
+	taskList.innerHTML = '';
+	taskLeftTd.textContent = '0';
+	ID = 0;
+};
+
+const deleteSingleTask = e => {
+	if(e.target.closest('button').classList.contains('trash-btn')){
+        e.target.closest('li').remove()
+        ID--;
+        taskLeftTd.textContent = ID;
+    }
 };
 
 addNewTaskBtn.addEventListener('click', addNewTask);
 discardBtn.addEventListener('click', addNewTask);
 saveBtn.addEventListener('click', createNewTask);
+deleteAllTasksBtn.addEventListener('click', deleteAllTasks);
+checkBtn.addEventListener('click', () => {
+	console.log(checkBtn);
+});
+
+document.addEventListener('click', deleteSingleTask);
